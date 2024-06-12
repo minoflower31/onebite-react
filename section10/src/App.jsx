01a3ca8eb9daf553a873deb0c08f2f7482d466f1
minoflower <minoflower31@naver.com>
2024-06-13 00:08:44 +0900

    Section10 useMemo 사용

---
+++ section10/src/App.jsx
@@ -0,0 +1,100 @@
+import "./App.module.css";
+
+import style from "./App.module.css";
+import { useReducer, useRef } from "react";
+import Header from "./components/Header.jsx";
+import Editor from "./components/Editor.jsx";
+import List from "./components/List.jsx";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "Docker 인강듣기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "데이트하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: false,
+    content: "일기 쓰기",
+    date: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "C":
+      return [...state, action.data];
+    case "U": {
+      return state.map((todo) =>
+        todo.id === action.data
+          ? {
+              ...todo,
+              isDone: !todo.isDone,
+            }
+          : todo,
+      );
+    }
+    case "D": {
+      return state.filter((todo) => todo.id !== action.data);
+    }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(todos.length);
+
+  const onCreate = (content) => {
+    dispatch({
+      type: "C",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  };
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: "U",
+      data: targetId,
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "D",
+      data: targetId,
+    });
+  };
+
+  return (
+    <>
+      <div className={style.container}>
+        {/*<Exam />*/}
+        <Header />
+        <Editor onCreate={onCreate} />
+        <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ section10/src/components/List.jsx
@@ -0,0 +1,69 @@
+import style from "./List.module.css";
+import TodoItem from "./TodoItem.jsx";
+import { useMemo, useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  // const getAnalyzedData = () => {
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo) => todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+  //
+  //   return { totalCount, doneCount, notDoneCount };
+  // };
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+
+  return (
+    <div className={style.container}>
+      <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>not done: {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className={style.todoWrapper}>
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
